72129d9477834e0efc6da9804a50298754daa4ec
HilaryEmmanuel <emmanuelhilary9@gmail.com>
2023-11-11 16:03:57 +0100

    connecting with eternal database

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
+// const dbConnection = new Sequelize(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,{ logging : false})
 const dbConnection = new Sequelize(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,{ logging : false})
 module.exports = { dbConnection }
